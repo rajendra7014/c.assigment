414cec6ce3e00f8d7134afce783bb089c464ed37
rajendra7014 <rsd44208@gmail.com>
2022-08-08 18:31:05 +0530

    rajendra

---
+++ while.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	x=1;
+	while(x<=10)
+	{
+		printf("\nhii...");
+		x++;
+	}          
+	do
+	{
+		printf("\nhello...");
+		x++;
+		
+	}
+	while(x<=10);
+	
+	
+	
+	
+	return 0;
+}
